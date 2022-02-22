852b13551d6b64ad876d9e316a1bbfcd7b5d5031
rmacaw <11528132+bmacaw@users.noreply.github.com>
2022-02-22 10:21:44 -0800

    Final Commit

--- torus_field_trip.py
+++ torus_field_trip.py
@@ -1,8 +1,12 @@
+# https://en.wikipedia.org/wiki/Torus
+# A torus "is similar in structure to a fractal as it is constructed by repeatedly
+# corrugating an ordinary torus. Like fractals, it has no defined Gaussian curvature.
+# However, unlike fractals, it does have defined surface normals."
+
 # Code from https://scipython.com/book/chapter-7-matplotlib/examples/a-torus/
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # TODO: why is this import not used?
 
 n = 100
 
@@ -21,13 +25,13 @@ fig = plt.figure()
 ax1 = fig.add_subplot(121, projection='3d')
 ax1.set_zlim(-3, 3)
 
-# We can use keywords such as edgecolors to style the polygon patched created
-# by ax.plot_surface
-ax1.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
+# We can use keywords such as edgecolors to style the polygon patches created
+# by ax.plot_surfacecolors
+ax1.plot_surface(x, y, z, rstride=5, cstride=5, color='#ADD8E6', edgecolors='blue')
 ax1.view_init(36, 26)
 
 ax2 = fig.add_subplot(122, projection='3d')
-ax2.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
+ax2.plot_surface(x, y, z, rstride=5, cstride=5, color='#ADD8E6', edgecolors='blue')
 ax1.view_init(0, 0)
 ax2.set_xticks([])
 plt.show()
