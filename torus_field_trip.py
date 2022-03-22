771bf5fc11f6c459d1a4fdb9c2bd81b6d99cb3c4
rmacaw <11528132+bmacaw@users.noreply.github.com>
2022-03-21 22:37:57 -0700

    Play with dimensions

--- torus_field_trip.py
+++ torus_field_trip.py
@@ -16,14 +16,14 @@ phi = np.linspace(0, 2.*np.pi, n)
 # We need theta and phi to range over the interval (0, 2pi) independently, so
 # use a meshgrid
 theta, phi = np.meshgrid(theta, phi)
-c, a = 2, 1
+c, a = 4, 2
 x = (c + a*np.cos(theta)) * np.cos(phi)
 y = (c + a*np.cos(theta)) * np.sin(phi)
 z = a * np.sin(theta)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(121, projection='3d')
-ax1.set_zlim(-3, 3)
+ax1.set_zlim(-4, 4)
 
 # We can use keywords such as edgecolors to style the polygon patches created
 # by ax.plot_surfacecolors
